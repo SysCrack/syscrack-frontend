aeb64e0640998577658f6fdcd8bbcc9880d67849
Mmanuel <adebayolashe@gmail.com>
2026-02-02 08:13:47 +0100

    npm build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen font-sans`}>
-        <ClientProviders>
-          {children}
-        </ClientProviders>
+        {children}
       </body>
     </html>
   );
